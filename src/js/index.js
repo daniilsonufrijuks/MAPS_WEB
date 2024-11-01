21cd8fb94de586f9c392d917da3b08d0f94cc372
danilavasiljevs <kombusted@gmail.com>
2024-11-01 14:00:01 +0000

    js map

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,9 @@
-/* eslint-disable no-undef */
-/**
- * Simple map
- */
+var map = L.map('map').setView([56.946285, 24.105078], 13);
 
-// config map
-let config = {
-    minZoom: 7,
-    maxZoom: 18,
-  };
-  // magnification with which the map will start
-  const zoom = 18;
-  // co-ordinates
-  const lat = 52.22977;
-  const lng = 21.01178;
-  
-  // calling map
-  const map = L.map("map", config).setView([lat, lng], zoom);
-  
-  // Used to load and display tile layers on the map
-  // Most tile servers require attribution, which you can set under `Layer`
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
